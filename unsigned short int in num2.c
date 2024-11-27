95171f32688153078f095b4cd72f1d4ed36f0bd5
jhutika <jyellapu@student.gitam.edu>
2024-11-27 09:01:02 +0530

    unsigned short int in num2.c

---
+++ unsigned short int in num2.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    unsigned short int num = 65535;
+    printf("size of the unsigned short int=%d\n", sizeof(num));
+    printf("Value =%d",num);
+    
+    return 0;
+    
+}
+/*/tmp/G6ogdA2NgS.o
+size of the unsigned short int=2
+Value =65535
+
+=== Code Execution Successful ===*/
