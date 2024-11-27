6af3be6ad6fc7c4c18b23c9da5937d287a4b2574
jhutika <jyellapu@student.gitam.edu>
2024-11-27 08:48:49 +0530

    number conversion-2.c

---
+++ number conversion-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2024112431;
+    printf("Decimal representation: %d \nOctal representation: %o\nHexademical representation: %x", a, a, a);
+
+    return 0;
+}
+/*Decimal representation: 2024112431 
+Octal representation: 17051300457
+Hexademical representation: 78a5812f*/
