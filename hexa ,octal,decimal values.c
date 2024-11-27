237870c370739a231452514836a783687ca1a4da
jhutika <jyellapu@student.gitam.edu>
2024-11-27 08:37:39 +0530

    hexa ,octal,decimal values.c

---
+++ hexa ,octal,decimal values.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a =4567;
+    int b =23;
+    printf("Decimal representation: %d \nOctal representation: %o\nHexademical representation: %x", a, a, a);
+    printf("\nDecimal representation: %d \nOctal representation: %o\nHexademical representation: %x", b, b, b);
+
+    return 0;
+}
+/*Decimal representation: 4567 
+Octal representation: 10727
+Hexademical representation: 11d7
+Decimal representation: 23 
+Octal representation: 27
+Hexademical representation: 17
+
+=== Code Execution Successful ===*/
